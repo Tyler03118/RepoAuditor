b16753e0fd76306d0a84468cee65ec064158f4eb
ziji <zli2296@wisc.edu>
2025-03-04 22:47:44 -0500

    fork push

---
+++ src/RepoAuditor/Plugins/GitHubCustomization/__init__.py
@@ -0,0 +1,12 @@
+# -------------------------------------------------------------------------------
+# |
+# |  Copyright (c) 2024 Scientific Software Engineering Center at Georgia Tech
+# |  Distributed under the MIT License.
+# |
+# -------------------------------------------------------------------------------
+"""Exports the GitHubCustomizationModule class"""
+
+from .Module import GitHubCustomizationModule
+
+# Export the module class
+MODULE_CLASS = GitHubCustomizationModule 
